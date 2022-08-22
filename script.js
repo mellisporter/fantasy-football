bf9fd7c2ef5e23e254e21f5599415c3e84be391b
Matt Porter <matthew.ellis.porter@gmail.com>
2022-08-22 11:11:01 -0400

    add event listener and api links

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 // alert('script is connected')
+
+// Finding the API endpoints that I need for this project
+
+let rankingURL = "https://api.fantasynerds.com/v1/nfl/draft-rankings?apikey=TEST&format="
+
+console.log(rankingURL)
+
+let adpURL = "https://api.fantasynerds.com/v1/nfl/adp?apikey=TEST=&format="
+
+console.log(adpURL)
+
+let newsURL = "https://api.fantasynerds.com/v1/nfl/news?apikey=TEST"
+
+console.log(newsURL)
+
+let playersURL = "https://api.fantasynerds.com/v1/nfl/players?apikey=TEST&include_inactive="
+
+console.log(playersURL)
+
+const $form = $('form')
+const $input = $('input')
+
+$form.on("submit", getHandleData)
+
+function getHandleData (event) {
+    event.preventDefault()
+    console.log($input.val())
+}
